465ddd594931a26a90d765520f16b2d741acea64
NajranAlFaresy <najranalfaresy06@gmail.com>
2025-02-20 16:36:06 +0700

    commit Latihan pertemuan

---
+++ src/pertemuan3/StrukturList.java
@@ -0,0 +1,60 @@
+package pertemuan3;
+
+public class StrukturList {
+	  private Node HEAD;
+
+	    public StrukturList() {
+	        this.HEAD = null;
+	    }
+
+	    // Fungsi untuk mengecek apakah list kosong
+	    
+	    public boolean isEmpty() {
+	        return HEAD == null;
+	    }
+
+	    // Fungsi untuk menambahkan elemen di akhir list (addTail) (Latihan 2)
+	    
+	    public void addTail(int data) {
+	    	Node posNode=null, curNode=null;
+	        Node newNode = new Node(data);
+	        if (isEmpty()) {
+	            HEAD = newNode;
+	        } 
+	        else 
+	        {
+	            curNode = HEAD;
+	            while (curNode != null) {
+	            	posNode = curNode;
+	                curNode = curNode.getNext();
+	            }
+	            posNode.setNext(newNode);
+	        }
+	    }
+	    
+
+	 // Fungsi untuk menambahkan elemen di awal list (addHead) (Latihan 5)
+	    
+	     public void addHead(int data) {
+	     Node newNode = new Node(data);
+	     if (isEmpty()) {
+	         HEAD = newNode;
+	     } else {
+	         newNode.setNext(HEAD);
+	         HEAD = newNode;
+	     }
+	 }
+
+	     
+	 // (Latihan 3)
+	 // Fungsi untuk menampilkan elemen list
+	     
+	     public void displayElement() {
+	     Node curNode = HEAD;
+	     while (curNode != null) {
+	         System.out.print(curNode.getData() + " ");
+	         curNode = curNode.getNext();
+	     }
+	     System.out.println();
+	 }
+}
